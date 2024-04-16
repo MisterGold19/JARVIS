e2ff9acb01889f6903c8684a2465625535ea6b46
MisterGold19 <ivan.kuzenkov.public@gmail.com>
2024-04-16 21:11:08 +0200

    add welcome page and test pages for signing up and in

--- JARVIS_APP.py
+++ JARVIS_APP.py
@@ -1,9 +1,17 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/sign_in')
+def sign_in_user():
+    return render_template('sign_in.html')
 
+@app.route('/sign_up')
+def sign_up_user():
+    return render_template('sign_up.html')
